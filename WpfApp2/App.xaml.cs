a48ac3baf5073645b1921954e138b76e655ba200
262038 <262038@WINMAC-4>
2023-10-11 18:11:52 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp2.Components;
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static NbaBase3Entities DB = new NbaBase3Entities();
+        public static Admin LoggedEmployee;
+        public static bool IsAutorizate = false;
+    }
+}
---
+++ WpfApp2/Pages/MainPages.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPages.xaml
+    /// </summary>
+    public partial class MainPages : Page
+    {
+        public MainPages()
+        {
+            InitializeComponent();
+        }
+
+        private void BtTeams_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtPlayers_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtMatcups_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtPhotos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
